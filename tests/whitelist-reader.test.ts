16ea374e51922ac8db240987e9e16e73d17d945d
Stanislas Bernard <bernardstanislas@gmail.com>
2022-07-02 16:49:31 +0200

    test: add a check for the computed nft types

--- tests/whitelist-reader.test.ts
+++ tests/whitelist-reader.test.ts
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {unpackNftTypes} from '../src/whitelist/reader';
+import {readWhitelist, unpackNftTypes} from '../src/whitelist/reader';
 
 describe('The whitelist reader', () => {
   it('decomposes the packed NFT types from the whitelist', async () => {
@@ -7,5 +7,16 @@ describe('The whitelist reader', () => {
     const nftTypes = unpackNftTypes(packedNftTypes);
 
     expect(nftTypes).to.deep.equal([0, 1, 2, 3]);
+  });
+
+  it('reads the correct types from the whitelist', async () => {
+    const whitelistPath = 'tests/test-whitelist.json';
+    const whitelist = await readWhitelist(whitelistPath);
+
+    const firstAddress = '0xDEC21982F07E8bff9b4c8FA8d18e550De0FACDDd';
+    const firstNftTypes = whitelist.find(
+      entry => entry.address.toLowerCase() === firstAddress.toLocaleLowerCase()
+    )?.nftTypes;
+    expect(firstNftTypes).to.deep.equal([0, 1, 2, 3, 5]);
   });
 });
